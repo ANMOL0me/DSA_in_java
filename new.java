e1b5dac73bae345a6a3fc3013905855a26428842
Anmol <sonianmol0918@gmail.com>
2026-03-21 20:03:04 +0530

    -

--- new.java
+++ new.java
@@ -1,152 +1,111 @@
 import java.io.*;
-
 import java.awt.*;
+import java.awt.event.*;
 
-class StudentFile extends Frame{
+class StudentFile extends Frame implements ActionListener {
 
-//Defining window components
+    // Defining window components
+    TextField number, name, marks;
+    Button enter, done;
+    Label numLabel, nameLabel, markLabel;
+    DataOutputStream dos;
 
-TextField number, name, marks;
+    // Constructor
+    public StudentFile() {
+        super("Create Student File");
+    }
 
-Button enter, done;
+    // Setup the window
+    public void setup() {
+        setSize(400, 200);
+        setLayout(new GridLayout(4, 2));
 
-Label numLabel, namelabel, markLabel;
+        // Create components
+        number = new TextField(25);
+        numLabel = new Label("Roll Number");
 
-DataOutputStream dos ;
+        name = new TextField(25);
+        nameLabel = new Label("Student Name");
 
-//Initialize the Frame
+        marks = new TextField(25);
+        markLabel = new Label("Marks");
 
-public StudentFile()
-{
-super ("Create Student File");
+        enter = new Button("ENTER");
+        done = new Button("DONE");
+
+        // Add components
+        add(numLabel);
+        add(number);
+
+        add(nameLabel);
+        add(name);
+
+        add(markLabel);
+        add(marks);
+
+        add(enter);
+        add(done);
+
+        // Add event listeners
+        enter.addActionListener(this);
+        done.addActionListener(this);
+
+        setVisible(true);
+
+        // Open file
+        try {
+            dos = new DataOutputStream(new FileOutputStream("student.dat"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    // Write to file
+    public void addRecord() {
+        try {
+            int num = Integer.parseInt(number.getText());
+            double m = Double.parseDouble(marks.getText());
+
+            dos.writeInt(num);
+            dos.writeUTF(name.getText());
+            dos.writeDouble(m);
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        // Clear fields
+        number.setText("");
+        name.setText("");
+        marks.setText("");
+    }
+
+    // Cleanup
+    public void cleanup() {
+        if (!number.getText().equals("")) {
+            addRecord();
+        }
+        try {
+            dos.flush();
+            dos.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    // Handle button clicks
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == enter) {
+            addRecord();
+        } else if (e.getSource() == done) {
+            cleanup();
+            System.exit(0);
+        }
+    }
+
+    // Main method
+    public static void main(String args[]) {
+        StudentFile student = new StudentFile();
+        student.setup();
+    }
 }
-// Setup the window
-
-public void setup()
-{
-resize (400, 200);
-
-setLayout (new GridLayout (4, 2));
-
-// Create the components of the Frame
-
-number = new TextField (25);
-
-numLabel = new Label ("Roll Number");
-
-name = new TextField (25);
-
-
-namelabel = new Label ("Student name") ;
-marks = new TextField (25) ;
-
-markLabel = new Label ("Marks") ;
-
-enter =  new Button ("ENTER");
-done = new Button("DONE") ;
-
-
-//Add the components to the Frame:
-
-add (numlabel) ;
-
-add (number) ;
-
-add (nameLabel);
-
-add (name);
-
-add (markLabel);
-
-add (marks);
-
-add (enter);
-
-add (done);
-
-//Show the Frame
-
-show () ;
-
-
-
-//Open the file
-try{
-dos = new DataOutputStream (new FileOutputStream ("student.dat")) ;
-
-catch (IOException e)
-
-{
-
-System.err.println (e.toString());
- System.exit (1);
-}}
-//Write to the file
-
-public void addRecord (){
-
-int num;
-
-Double d;
-
-num = (new Integer (number.getText())).intValue();
-try{
-dos.writeInt(num);
-
-dos.writeUTF(name.getText());
-
-d = new Double (marks.getText()) ;
-
-dos.writeDouble (d. doubleValue());
-
-catch (10Exception e) [
-
-Clear the text fields
-
-number.setText(""):
-
-name.setText ("");
-
-//Adding the record and clearing the TextFields
-
-public void cleanup()307
-
-if it number.getText().equals("")
-
-das.flush()
-
-dos.close();
-
-L catch (IOException e)(
-
-Processing the event
-
-public boolean action (Event event, obtect al
-
-if (event.teg instanceof Button)
-
-if (event.arg.equals ("ENTER"
-
-addRecord ( return true;
-
-return super.action (event, al
-
-public boolean handleEvent (Event event)
-
-if (event-get instanceof Button)
-
-if (event.arg.equals("DONE"))
-
-cleanup (1
-
-xit (0) 2
-
-System.exit return truer
-
-return super.handleEvent (event):
-
-Execute the program
-
-public static void main (String arge (1)
-
-Student File student student.setup() new StudentFile 11
