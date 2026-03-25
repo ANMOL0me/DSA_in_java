2f25ee95d9bbdb4e52c7143c6c7be14ae458dbce
Anmol <sonianmol0918@gmail.com>
2026-03-25 22:23:30 +0530

    Project 5

--- Petrol_Pump.java
+++ Petrol_Pump.java
@@ -1,44 +1,65 @@
-import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.*;
 
-class PetrolPump {
+public class Petrol_Pump extends Frame implements ActionListener {
+
+    TextField quantity;
+    TextArea receipt;
+    Button petrol, diesel;
+
+    double petrolPrice = 106.5;
+    double dieselPrice = 94.2;
+
+    Petrol_Pump() {
+
+        setTitle("Petrol Pump System");
+        setSize(400,300);
+        setLayout(new FlowLayout());
+
+        Label l1 = new Label("Enter Litres:");
+
+        quantity = new TextField(10);
+
+        petrol = new Button("Petrol");
+        diesel = new Button("Diesel");
+
+        receipt = new TextArea(8,30);
+
+
+        petrol.addActionListener(this);
+        diesel.addActionListener(this);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        double litres = Double.parseDouble(quantity.getText());
+        double amount;
+
+        if(e.getSource() == petrol) {
+
+            amount = litres * petrolPrice;
+
+            receipt.setText("Fuel : Petrol\n");
+            receipt.append("Price/Litre : " + petrolPrice + "\n");
+            receipt.append("Litres : " + litres + "\n");
+            receipt.append("Total Amount : ₹" + amount);
+        }
+
+        if(e.getSource() == diesel) {
+
+            amount = litres * dieselPrice;
+
+            receipt.setText("Fuel : Diesel\n");
+            receipt.append("Price/Litre : " + dieselPrice + "\n");
+            receipt.append("Litres : " + litres + "\n");
+            receipt.append("Total Amount : ₹" + amount);
+        }
+    }
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        double petrolPrice = 106.5;
-        double dieselPrice = 94.2;
-
-        int choice;
-        double liters, amount;
-
-        System.out.println("===== Welcome to Petrol Pump =====");
-        System.out.println("1. Petrol");
-        System.out.println("2. Diesel");
-        System.out.print("Select Fuel Type: ");
-        choice = sc.nextInt();
-
-        System.out.print("Enter Number of Liters: ");
-        liters = sc.nextDouble();
-
-        if (choice == 1) {
-            amount = liters * petrolPrice;
-            System.out.println("\nFuel Type : Petrol");
-            System.out.println("Price per Liter : " + petrolPrice);
-            System.out.println("Liters : " + liters);
-            System.out.println("Total Amount : ₹" + amount);
-        } 
-        else if (choice == 2) {
-            amount = liters * dieselPrice;
-            System.out.println("\nFuel Type : Diesel");
-            System.out.println("Price per Liter : " + dieselPrice);
-            System.out.println("Liters : " + liters);
-            System.out.println("Total Amount : ₹" + amount);
-        } 
-        else {
-            System.out.println("Invalid Choice!");
-        }
-
-        System.out.println("===== Thank You Visit Again =====");
+        new Petrol_Pump();
     }
 }
