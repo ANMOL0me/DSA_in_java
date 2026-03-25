1ecae9c1a515dd6a109f273c59b67f33cc7e4e90
Anmol <sonianmol0918@gmail.com>
2026-03-25 22:02:01 +0530

    Project 5

--- Petrol_Pump.java
+++ Petrol_Pump.java
@@ -8,14 +8,3 @@ class PetrolPump {
 
         double petrolPrice = 106.5;
         double dieselPrice = 94.2;
-
-        int choice;
-        double liters, amount;
-
-        System.out.println("===== Welcome to Petrol Pump =====");
-        System.out.println("1. Petrol");
-        System.out.println("2. Diesel");
-        System.out.print("Select Fuel Type: ");
-        choice = sc.nextInt();
-
- 
