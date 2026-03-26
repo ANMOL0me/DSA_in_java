83d9d45f7acde8f416200494c1e0fc2ee63bc6fd
Anmol <sonianmol0918@gmail.com>
2026-03-26 20:49:20 +0530

    -

--- SnakeGame.java
+++ SnakeGame.java
@@ -77,4 +77,105 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         appleY = random.nextInt((int)(HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
 
+    public void move() {
+
+        for(int i = bodyParts; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
  
+        }
+
+        switch(direction) {
+            case 'U': y[0] = y[0] - UNIT_SIZE; break;
+            case 'D': y[0] = y[0] + UNIT_SIZE; break;
+            case 'L': x[0] = x[0] - UNIT_SIZE; break;
+            case 'R': x[0] = x[0] + UNIT_SIZE; break;
+        }
+    }
+
+    public void checkApple() {
+        if((x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
+    }
+
+    public void checkCollisions() {
+
+        for(int i = bodyParts; i > 0; i--) {
+            if((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+
+        if(x[0] < 0) running = false;
+        if(x[0] > WIDTH) running = false;
+        if(y[0] < 0) running = false;
+        if(y[0] > HEIGHT) running = false;
+
+        if(!running) timer.stop();
+    }
+
+    public void gameOver(Graphics g) {
+
+        g.setColor(Color.red);
+        g.setFont(new Font("Arial",Font.BOLD,40));
+        g.drawString("Game Over",200,300);
+
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial",Font.BOLD,20));
+        g.drawString("Score: "+applesEaten,250,350);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+
+        repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+                if(direction!='R') direction='L';
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                if(direction!='L') direction='R';
+                break;
+
+            case KeyEvent.VK_UP:
+                if(direction!='D') direction='U';
+                break;
+
+            case KeyEvent.VK_DOWN:
+                if(direction!='U') direction='D';
+                break;
+        }
+    }
+
+    @Override public void keyReleased(KeyEvent e) {}
+    @Override public void keyTyped(KeyEvent e) {}
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        SnakeGame gamePanel = new SnakeGame();
+
+        frame.add(gamePanel);
+        frame.setTitle("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+}
