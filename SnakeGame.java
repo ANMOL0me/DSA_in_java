6f1c4e0c274c3e4d7e816f6d1caf6814f8b900f0
Anmol <sonianmol0918@gmail.com>
2026-03-26 20:43:56 +0530

    -

--- SnakeGame.java
+++ SnakeGame.java
@@ -3,3 +3,10 @@ import java.awt.event.*;
 import java.util.Random;
 import javax.swing.*;
 
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
+
+    static final int WIDTH = 600;
+    static final int HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (WIDTH * HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 100;
