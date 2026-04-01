9cfad0e87db1a3c8a993baf178d1b9b55e3266fd
Anmol <sonianmol0918@gmail.com>
2026-04-01 06:50:55 +0530

    -

--- Petrol_Pump.java
+++ Petrol_Pump.java
@@ -19,3 +19,53 @@ public class Petrol_Pump extends Frame implements ActionListener {
         Label l1 = new Label("Enter Litres:");
 
         quantity = new TextField(10);
+
+        petrol = new Button("Petrol");
+        diesel = new Button("Diesel");
+
+        receipt = new TextArea(8,30);
+
+        
+        add(petrol);
+        add(diesel);
+        add(l1);
+        add(quantity);
+        add(receipt);
+
+        petrol.addActionListener(this);
+        diesel.addActionListener(this);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        double litres = Double.parseDouble(quantity.getText());
+        double amount;
+
+        if(e.getSource() == petrol) {
+
+            amount = litres * petrolPrice;
+
+            receipt.setText("Fuel : Petrol\n");
+            receipt.append("Price/Litre : " + petrolPrice + "\n");
+            receipt.append("Litres : " + litres + "\n");
+            receipt.append("Total Amount : ₹" + amount);
+        }
+
+        if(e.getSource() == diesel) {
+
+            amount = litres * dieselPrice;
+
+            receipt.setText("Fuel : Diesel\n");
+            receipt.append("Price/Litre : " + dieselPrice + "\n");
+            receipt.append("Litres : " + litres + "\n");
+            receipt.append("Total Amount : ₹" + amount);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        new Petrol_Pump();
+    }
+}
