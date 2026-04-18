b5085fc5ce7a0059596e7294835c6772d32bf915
Anmol <sonianmol0918@gmail.com>
2026-04-19 00:41:44 +0530

    hanges

---
+++ main.java
@@ -0,0 +1,9 @@
+ public class main {
+    public static void main(String[] args)
+system.out.print("hello day 1");
+
+}  public class main {
+    public static void main(String[] args)
+system.out.print("hello day 1");
+
+}
