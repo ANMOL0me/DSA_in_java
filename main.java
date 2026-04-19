c35e1843478cd49f5e69f4b504158cb4017cae32
Anmol <sonianmol0918@gmail.com>
2026-04-20 04:15:34 +0530

    vhangess

--- main.java
+++
@@ -1,8 +0,0 @@
- public class main {
-    public static void main(String[] args)
-system.out.print("hello day 1");
-
-}  public class main {
-    public static void main(String[] args)
-system.out.print("hello day 1");
-
