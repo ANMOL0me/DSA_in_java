da229b3cf5daf598fe367f22a8364f0c4adcea3c
Anmol <sonianmol0918@gmail.com>
2026-04-21 23:15:54 +0530

    vhanjgjessss

---
+++ main.java
@@ -0,0 +1,11 @@
+public  public class main {
+    public static void main(String[] args)
+system.out.print("hello day 1");
+
+}  public class main {
+    public static void main(String[] args)
+system.out.print("hello day 1");
+
+ {
+    
+}
