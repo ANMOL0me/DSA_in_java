4bdd76a1e1d6a3136ae2b4614a22b8a671ce4a5e
ANMOL <sonianmol0918@gmail.com>
2026-07-24 20:43:03 +0530

    Single_inheritance

---
+++ inheritance.java
@@ -0,0 +1,17 @@
+class parent { //parent class
+public
+parent(){
+System.out.println("parent class function");
+}
+}
+class child extends parent { //child class
+public
+child(){
+System.out.println("Child class function");
+}
+}
+public class inheritance{
+public static void main(String[] args){
+child obj = new child() ;
+}
+} 
