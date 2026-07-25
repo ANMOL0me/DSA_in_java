a25c243d6c9ff6c0b7c618cf168774ee8699ed88
ANMOL <sonianmol0918@gmail.com>
2026-07-25 22:06:00 +0530

    multiple inheritance

---
+++ Multiple_inheritance.java
@@ -0,0 +1,17 @@
+interface face1{
+void fun1();}
+interface face2{
+void fun2();}
+class class1 implements face1,face2 {
+public void fun1(){
+System.out.println("interafce_1 function");
+}
+public void fun2(){
+System.out.println("interafce_2 function");
+}}
+
+public class Multiple_inheritance{
+public static void main(String[] args){
+class1 obj = new class1() ;
+face1 obj1 = new face1() ;
+}}
